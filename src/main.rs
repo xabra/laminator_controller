9187c9ed2c8f2b21d508218964537b6c25fcaf3a
Adam Brailove <adam.brailove@gmail.com>
2022-09-25 19:47:38 -0700

    added new rtic-monotonic for rp2040-hal & rp-pico

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,27 @@ use defmt_rtt as _;
 mod app {
 
     use embedded_hal::digital::v2::OutputPin;
-    use rp2040_monotonic::*;
-    use fugit::{MicrosDurationU64};
+    use fugit::{MicrosDurationU32,MicrosDurationU64};
     use defmt::*;
     use rp_pico::{
-        hal::{self, clocks::init_clocks_and_plls, watchdog::Watchdog, Sio},
+        hal::{self, clocks::init_clocks_and_plls, timer::{monotonic::Monotonic, Alarm, Alarm0}, watchdog::Watchdog, Sio},
         XOSC_CRYSTAL_FREQ,
     };
 
     // PWM timebase
     const PWM_PERIOD: MicrosDurationU64  = MicrosDurationU64::millis(2000);
+    const SCAN_TIME_US: MicrosDurationU32 = MicrosDurationU32::secs(1);
 
     #[monotonic(binds = TIMER_IRQ_0, default = true)]
-    type MyMono = Rp2040Monotonic;
+    type MyMono = Monotonic<Alarm0>;
 
     #[shared]
     struct Shared {
-        master_cycle_trigger_pin: hal::gpio::Pin<hal::gpio::pin::bank0::Gpio8, hal::gpio::PushPullOutput>,  // Debug only.
-        htr_ctr_pin: hal::gpio::Pin<hal::gpio::pin::bank0::Gpio15, hal::gpio::PushPullOutput>,
-        htr_ctr_pulse_width: MicrosDurationU64,
+        timer: hal::Timer,
+        alarm1: hal::timer::Alarm1,     // HW IRQ 
+        //alarm2: hal::timer::Alarm2,   // Unused for now
+        //alarm3: hal::timer::Alarm3,   // Unused for now
+        measure_pin: hal::gpio::Pin<hal::gpio::pin::bank0::Gpio8, hal::gpio::PushPullOutput>,
     }
 
     #[local]
@@ -60,57 +62,42 @@ mod app {
             sio.gpio_bank0,
             &mut resets,
         );
-        // Create new monotonic
-        let mono = Rp2040Monotonic::new(c.device.TIMER);
+        // Create new timer and alarm
+        let mut timer = hal::Timer::new(c.device.TIMER, &mut resets);
+        let alarm0 = timer.alarm_0().unwrap();          // Alarm0 reserved for monotonic/ SW tasks
+        let mut alarm1 = timer.alarm_1().unwrap();      // Use for HW IRQ Task
+        let mut _alarm2 = timer.alarm_2().unwrap();      // Unused for now
+        let mut _alarm3 = timer.alarm_3().unwrap();      // Unused for now
 
+        let mut measure_pin = pins.gpio8.into_push_pull_output();
+        measure_pin.set_low().unwrap();
 
-        // Heater PWMs setup
-        let mut htr_ctr_pin = pins.gpio15.into_push_pull_output();
-        htr_ctr_pin.set_low().unwrap();
-  
-        // Init the pwm pulse width
-        let htr_ctr_pulse_width = MicrosDurationU64::millis(200);
+        // Fast measurement HW interrupt
+        let _ = alarm1.schedule(SCAN_TIME_US);
+        alarm1.enable_interrupt();
 
-        let mut master_cycle_trigger_pin = pins.gpio8.into_push_pull_output();
-        master_cycle_trigger_pin.set_low().unwrap();
-
-        pwm_period_timer::spawn().unwrap();
-        sample_timer::spawn().unwrap();
-
-        (Shared { htr_ctr_pulse_width, htr_ctr_pin, master_cycle_trigger_pin}, Local {}, init::Monotonics(mono))
+        (Shared { timer, alarm1, measure_pin}, Local {}, init::Monotonics(Monotonic::new(timer, alarm0)))
     }
 
     // -- TASK: High speed measurement & processing cycle start
-    #[task(priority = 3, shared = [master_cycle_trigger_pin])]
+    #[task(
+        priority = 1, 
+        binds = TIMER_IRQ_1,
+        local = [tog: bool = true], 
+        shared = [measure_pin, alarm1])]
     fn sample_timer(mut c: sample_timer::Context) { 
+        if *c.local.tog {
+            c.shared.measure_pin.lock(|l| l.set_high().unwrap());
+        } else {
+            c.shared.measure_pin.lock(|l| l.set_low().unwrap());
+        }
+        *c.local.tog = !*c.local.tog;
 
-        c.shared.master_cycle_trigger_pin.lock(|l| l.set_high().unwrap()); 
-        sample_timer::spawn_after(1.millis()).unwrap();
-        c.shared.master_cycle_trigger_pin.lock(|l| l.set_low().unwrap()); 
-    }
+        let mut alarm = c.shared.alarm1;
+        (alarm).lock(|a| {
+            a.clear_interrupt();
+            let _ = a.schedule(SCAN_TIME_US);
+        });
 
-    // -- TASK: Low speed PWM period timebase
-    #[task(priority = 1, shared = [htr_ctr_pulse_width, htr_ctr_pin])]
-    fn pwm_period_timer(mut c: pwm_period_timer::Context) { 
-
-        // Schedule a restart of this task first 
-        pwm_period_timer::spawn_after(PWM_PERIOD).unwrap();
-        
-        // Get convenient pwm output pin and pulse width variables
-        let ctr  = c.shared.htr_ctr_pin;
-        let pw = c.shared.htr_ctr_pulse_width;
-
-        // Lock the shared values and do critical stuff in closure.
-        // Set pins high to start duty factor pulse and schedule a task to end the duty-factor pulse
-        (ctr, pw).lock(|ctr, pw| 
-            {ctr.set_high().unwrap();
-            pwm1_set_low::spawn_after(*pw).unwrap();}
-        ); 
-    }
-
-    // -- TASK: End the PWM duty-factor pulse
-    #[task(priority = 1, shared = [htr_ctr_pin])]
-    fn pwm1_set_low(mut c: pwm1_set_low::Context) {
-        c.shared.htr_ctr_pin.lock(|l| l.set_low().unwrap());
     }
 }
