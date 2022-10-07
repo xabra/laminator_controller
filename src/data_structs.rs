69a8ae75cd3433976caf742c08c720a89ae59424
Adam Brailove <adam.brailove@gmail.com>
2022-10-07 09:17:59 -0700

    add TCError s to data struct

--- src/data_structs.rs
+++ src/data_structs.rs
@@ -1,11 +1,16 @@
+use crate::thermocouple_controller::TCError;
 
+use defmt_rtt as _;
 
+#[derive(Debug, Copy, Clone)]
 pub struct Measurement {
     pub temp_ctr: f32,
     pub temp_lr: f32,
-    pub temp_fb: f32,   
+    pub temp_fb: f32, 
+    pub temp_err_ctr: TCError,
+    pub temp_err_lr: TCError,
+    pub temp_err_fb: TCError,  
     pub p_chamber: f32,
     pub p_bladder: f32,
 }
 
-//type Measurements = Measurements;
