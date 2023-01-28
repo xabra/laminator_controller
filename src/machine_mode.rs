652e6aa810bab177ea0a39245632c0f70717f96a
Adam Brailove <adam.brailove@gmail.com>
2023-01-28 07:02:06 -0800

    Simplify macine mode enum names

--- src/machine_mode.rs
+++ src/machine_mode.rs
@@ -3,9 +3,9 @@ use defmt_rtt as _;
 
 #[derive(Debug, Serialize, Clone, Copy, Deserialize, defmt::Format, PartialEq)]
 pub enum MachineMode {
-    mode_auto,
-    mode_manual,
-    mode_stopped,
+    Auto,
+    Manual,
+    Stopped,
 }
 
 pub struct MachineModeController {
@@ -16,43 +16,43 @@ impl MachineModeController {
     // Initialize
     pub fn new() -> MachineModeController {
         MachineModeController {
-            mode: MachineMode::mode_stopped,
+            mode: MachineMode::Stopped,
         }
     }
 
     // Start recipe, go to auto mode
     pub fn start(&mut self) {
-        if self.mode == MachineMode::mode_manual {
+        if self.mode == MachineMode::Manual {
             //   TODO initi recipe here...
-            self.mode = MachineMode::mode_auto;
+            self.mode = MachineMode::Auto;
         }  
     }
 
     // Stop recipe and go to manual mode
     pub fn stop(&mut self) {
-        if self.mode == MachineMode::mode_auto {
+        if self.mode == MachineMode::Auto {
             //   TODO half recipe here...
-            self.mode = MachineMode::mode_manual;
+            self.mode = MachineMode::Manual;
         }  
     }
 
     // Power off
     pub fn off(&mut self) {
-        if self.mode == MachineMode::mode_manual || self.mode == MachineMode::mode_auto {
+        if self.mode == MachineMode::Manual || self.mode == MachineMode::Auto {
             //   TODO ..
-            self.mode = MachineMode::mode_stopped;
+            self.mode = MachineMode::Stopped;
         }  
     }
 
     // Power on, go to manual mode.
     pub fn on(&mut self) {
-        if self.mode == MachineMode::mode_stopped {
+        if self.mode == MachineMode::Stopped {
             //   TODO ..
-            self.mode = MachineMode::mode_manual;
+            self.mode = MachineMode::Manual;
         }  
     }
 
-    pub fn get_modes(self) -> MachineMode {
+    pub fn get_mode(self) -> MachineMode {
         self.mode
     }
 
